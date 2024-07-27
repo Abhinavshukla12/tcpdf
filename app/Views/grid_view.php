caf31b5887c3f6371ab528aca19f7dda126b0eb5
Abhinav shukla <124790218+Abhinavshukla12@users.noreply.github.com>
2024-07-27 21:08:44 +0530

    bootstrap css added

--- app/Views/grid_view.php
+++ app/Views/grid_view.php
@@ -2,32 +2,35 @@
 <html>
 <head>
     <title>Grid Data</title>
+    <!-- Include Bootstrap CSS -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <!-- Include jsPDF library -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.4.0/jspdf.umd.min.js"></script>
 </head>
 <body>
-    <h1>Grid Data</h1>
-    <table border="1" id="gridTable">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Value</th>
-            </tr>
-        </thead>
-        <tbody id="gridBody">
-            <?php foreach ($gridData as $row): ?>
+    <div class="container">
+        <h1 class="my-4">Grid Data</h1>
+        <table class="table table-bordered" id="gridTable">
+            <thead class="thead-dark">
                 <tr>
-                    <td><?= $row['id'] ?></td>
-                    <td><?= $row['name'] ?></td>
-                    <td><?= $row['value'] ?></td>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Value</th>
                 </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
-    <button onclick="generatePDF()">Generate PDF</button>
-
-    <div id="pdfLink" style="margin-top: 20px;"></div>
+            </thead>
+            <tbody id="gridBody">
+                <?php foreach ($gridData as $row): ?>
+                    <tr>
+                        <td><?= $row['id'] ?></td>
+                        <td><?= $row['name'] ?></td>
+                        <td><?= $row['value'] ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+        <button class="btn btn-primary" onclick="generatePDF()">Generate PDF</button>
+        <div id="pdfLink" style="margin-top: 20px;"></div>
+    </div>
 
     <script>
         async function generatePDF() {
